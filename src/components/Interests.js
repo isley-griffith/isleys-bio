1744b09fe7b57800c8fe4742a624a4699da21c07
Isley Griffith <isleygriffith@gmail.com>
2022-06-21 14:00:57 -0700

    Added another interest, finished color theme

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { FaGuitar } from "react-icons/fa";
-import { GrDesktop } from "react-icons/gr";
+import { GiCookingPot } from "react-icons/gi";
 import { AiOutlineInstagram } from "react-icons/ai";
+import { BiDesktop } from "react-icons/bi";
+import { IconContext } from "react-icons";
+
 function Interests() {
   return (
     <div className="max-w-4xl mx-auto justify-center py-12" id="interests">
@@ -15,7 +18,9 @@ function Interests() {
       <div className="flex flex-wrap justify-center pt-2">
         <div className="flex flex-col w-40 p-10 m-6 overflow-hidden bg-white shadow-2xl rounded-xl sm:w-52">
           <div className="flex">
-            <FaGuitar className="mx-auto text-4xl" />
+            <IconContext.Provider value={{ color: "#F1F1F9" }}>
+              <FaGuitar className="mx-auto text-4xl" />
+            </IconContext.Provider>
           </div>
           <p
             style={{ color: "#F1F1F9" }}
@@ -24,20 +29,38 @@ function Interests() {
             Guitar
           </p>
           <a href="https://www.instagram.com/isleyguitar/">
-            <AiOutlineInstagram
-              color="#2196f3"
-              className="mx-auto text-4xl"
-              onClick={() => {}}
-            />
+            <IconContext.Provider value={{ color: "#F1F1F9" }}>
+              <AiOutlineInstagram
+                className="mx-auto text-4xl"
+                onClick={() => {}}
+              />
+            </IconContext.Provider>
           </a>
         </div>
-        <div className="flex flex-col w-40  p-10 m-6 overflow-hidden bg-white shadow-2xl rounded-xl sm:w-52">
-          <GrDesktop color="white" className="mx-auto text-4xl" />
+        <div className="flex flex-col w-40 p-10 m-6 overflow-hidden bg-white shadow-2xl rounded-xl sm:w-52">
+          <div className="flex">
+            <IconContext.Provider value={{ color: "#F1F1F9" }}>
+              <BiDesktop className="mx-auto text-4xl" />
+            </IconContext.Provider>
+          </div>
           <p
             style={{ color: "#F1F1F9" }}
             className="mt-6 text-xl font-semibold text-center font-mono"
           >
             Gaming
+          </p>
+        </div>
+        <div className="flex flex-col w-40 p-10 m-6 overflow-hidden bg-white shadow-2xl rounded-xl sm:w-52">
+          <div className="flex">
+            <IconContext.Provider value={{ color: "#F1F1F9" }}>
+              <GiCookingPot className="mx-auto text-4xl" />
+            </IconContext.Provider>
+          </div>
+          <p
+            style={{ color: "#F1F1F9" }}
+            className="mt-6 text-xl font-semibold text-center font-mono"
+          >
+            Cooking
           </p>
         </div>
       </div>
